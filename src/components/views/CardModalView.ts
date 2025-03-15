9021e4e4eb58dac46da6fbc387da96ed7e04d3a8
badabumb <juliavediukova@mail.ru>
2025-03-15 13:38:11 +0300

    Добавить CardModalView

---
+++ src/components/views/CardModalView.ts
@@ -0,0 +1,38 @@
+import { IActions, IProduct } from "../../types";
+import { IEvents } from "../base/events";
+import { CardView } from "./CardView";
+
+export interface ICardModalView {
+    description: HTMLElement;
+    button: HTMLElement;
+    
+    render(data: IProduct): HTMLElement;
+}
+
+export class CardModalView extends CardView implements ICardModalView {
+    description: HTMLElement;
+    button: HTMLElement;
+
+    constructor(
+        template: HTMLTemplateElement,
+        protected events: IEvents,
+        actions?: IActions,
+    ) {
+        super(template, events, actions);
+        this.description = this._cardElement.querySelector(".card__text");
+        this.button = this._cardElement.querySelector(".card__button");
+        this.button.addEventListener("click", () => {
+            this.events.emit("card:addBasket");
+        });
+    }
+
+    render(data: IProduct): HTMLElement {
+        super.render(data);
+        this.description.textContent = data.description;
+        if (!data.price) {
+            this.button.setAttribute("disabled", "true");
+        }
+
+        return this._cardElement;
+    }
+}

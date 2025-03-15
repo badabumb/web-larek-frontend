db2e58857f2470d31d7a406bb591220ba5bfbb61
badabumb <juliavediukova@mail.ru>
2025-03-15 12:21:46 +0300

    Добавить BasketModel

---
+++ src/components/models/BasketModel.ts
@@ -0,0 +1,54 @@
+import { IProduct } from "../../types";
+
+export interface IBasketModel {
+    basketProducts: IProduct[];
+    getAmount: () => number;
+    getTotalCost: () => number;
+    addToBasket(data: IProduct): void;
+    deleteFromBasket(item: IProduct): void;
+    clearBasket(): void;
+}
+
+export class BasketModel implements IBasketModel {
+    protected _basketProducts: IProduct[];
+
+    constructor() {
+        this._basketProducts = [];
+    }
+
+    set basketProducts(data: IProduct[]) {
+        this.basketProducts = data;
+    }
+
+    get basketProducts() {
+        return this._basketProducts;
+    }
+
+    getAmount() {
+        return this.basketProducts.length;
+    }
+
+    getTotalCost() {
+        let total = 0;
+        this.basketProducts.forEach(item => {
+            total += item.price;
+        });
+        return total;
+    }
+
+    addToBasket(data: IProduct) {
+        this._basketProducts.push(data);
+    }
+
+    deleteFromBasket(item: IProduct) {
+        const index = this.basketProducts.indexOf(item);
+        
+        if (index >= 0) {
+            this._basketProducts.splice(index, 1);
+        }
+    }
+
+    clearBasket() {
+        this.basketProducts = [];
+    }
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-export interface IProductItem {
+export interface IProduct {
     id: string;
     description: string;
     image: string;
